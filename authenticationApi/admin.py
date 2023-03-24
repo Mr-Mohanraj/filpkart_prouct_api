ddf79279731f111b02082133ccd3dc793a1c6207
Mohanraj <93506428+Mr-Mohanraj@users.noreply.github.com>
2023-03-24 18:26:07 +0530

    requirements.txt update

--- authenticationApi/admin.py
+++ authenticationApi/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import ApiAccessToken, Reset, UserToken,User
 
 admin.site.register((ApiAccessToken, Reset, UserToken,User))
--- authenticationApi/authentication.py
+++ authenticationApi/authentication.py
@@ -1,5 +1,3 @@
-import jwt
-import datetime
 from rest_framework import exceptions
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.authentication import get_authorization_header
@@ -8,22 +6,6 @@ from .utils import decode_access_token
 
 
 class JWTAuthentication(BaseAuthentication):
-    """Use to find the user is already register or not. based on the token in cookies inside the browsers"""
-    def authenticate(self, request):
-        auth = get_authorization_header(request=request).split()
-
-        if auth and len(auth) == 2:
-            token = auth[1].decode('utf-8')
-            id = decode_access_token(token)
-
-            user = User.objects.get(pk=id)
-
-            return (user, None)
-
-        raise exceptions.AuthenticationFailed('Unauthenticated')
-
-
-class DeveloperJWTAuthentication(BaseAuthentication):
     """Use to find the user is already register or not. based on the token in cookies inside the browsers"""
     def authenticate(self, request):
         auth = get_authorization_header(request=request).split()
